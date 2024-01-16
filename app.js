9c5c098314ae7d49661e7fc3e4e9cfca00c96ebc
Victor Salinas <victorsalinas6@gmail.com>
2024-01-16 14:55:07 -0600

    Finished the variables exercise

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 && 2
 
-// Exercise 2
+const firstName = "Victor"
+let lastName = "Salinas"
+var age = 30;
 
-// Exercise 3
+console.log(firstName,lastName,age)
+
+// Exercise 4
+
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+console.log(language,createdYear,isCaseSensitive)
+
+// Exercise 4
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+console.log(price,isOnSale,salePercentage,stock,inStock,selectedSize)
+
+//Exercise 5
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+
+console.log(title,author,pageCount,bookmark,hasRead)
